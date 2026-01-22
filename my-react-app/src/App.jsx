0fe363c08596283b73dc8a41907b570aeb28e6b3
Joanna Itzel Perez Ordaz <240222@utxicotepec.edu.mx>
2026-01-22 14:21:03 -0600

    add: se agregaron funciones y componentes con una lista de usuarios y materiales

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,65 @@
 function App() {
+ 
   return (
   <div>
   <h1>5A EVND</h1>
   <h2>Profesor:</h2>
   <h3>M.T.I Ricardo Luna Santos</h3>
-  <h3>Alumnos</h3>
-  <h4>Joanna</h4>
-  <h4>Itzel</h4>
+  <UserComponent/>
+  <ProfileComponent/>
+  <FeedComponent/>
   </div>
+  )
+}
+
+function UserComponent(){
+  const nombre='Joanna';
+  const apellidos='Perez';
+  const nombrecompleto= <h2>El nombre es: {nombre} y sus apellidos {apellidos}</h2>;
+  return <h2>User Component {nombrecompleto}</h2>;
+}
+function ProfileComponent(){
+   const users= [
+    { id: 1, name:'Joanna', role: 'Web Developer'},
+    { id: 2, name:'Itzel', role: 'Web Desinger'},
+    { id: 3, name:'Vanesa', role: 'Team Leader'},
+   ]
+  return (
+    <>
+    <p>Lista de usuarios del sistema</p>
+    <ul>
+      {
+      users.map(function(user, index){
+        return (
+          <li key={index}>{user.name} es un {user.role}</li>
+        )
+      })
+    }
+    </ul>
+    </>
+  )
+}
+function FeedComponent(){
+  const materiales= [
+    { id: 1, name:'Cemento'},
+    { id: 2, name:'Arena'},
+    { id: 3, name:'Barilla'},
+    { id: 4, name:'Ladrillos'},
+    { id: 5, name:'Grava'},
+   
+   ]
+  return (
+    <>
+    <p>Lista de materiales</p>
+    <ul>
+      {materiales.map(function(material, index){
+      return (
+        <li key={index}>{material.name} </li>
+        )
+      })
+    }
+    </ul>
+    </>
   )
 }
 
