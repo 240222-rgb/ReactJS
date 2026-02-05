51711dcca9f0e602b5b74a491ecbd2091509983c
Joanna Itzel <240222@utxicotepec.edu.mx>
2026-02-05 14:20:37 -0600

    add: se agregaron componentes hijos

--- my-react-app/src/ContenedorTargeta.jsx
+++ my-react-app/src/ContenedorTargeta.jsx
@@ -12,32 +12,32 @@ function ContenedorTargeta() {
       <div className="Tarjeta">
         <img src={img1} alt="Tarjeta 1" />
         <div className="Tarjeta-contenido">
-          <h3>Título 1</h3>
-          <p>Texto de la tarjeta</p>
+          <h3>Super Gato 1</h3>
+          <p>El es el gato que piensa que es la monalisa</p>
         </div>
       </div>
 
       <div className="Tarjeta">
         <img src={img2} alt="Tarjeta 2" />
         <div className="Tarjeta-contenido">
-          <h3>Título 2</h3>
-          <p>Texto de la tarjeta</p>
+          <h3>Super Gato 2</h3>
+          <p>El gato privilegiado wueroo</p>
         </div>
       </div>
 
       <div className="Tarjeta">
         <img src={img3} alt="Tarjeta 3" />
         <div className="Tarjeta-contenido">
-          <h3>Título 3</h3>
-          <p>Texto de la tarjeta</p>
+          <h3>Super Gato 3</h3>
+          <p>El gato que no se rinde nunca</p>
         </div>
       </div>
 
       <div className="Tarjeta">
         <img src={img4} alt="Tarjeta 4" />
         <div className="Tarjeta-contenido">
-          <h3>Título 4</h3>
-          <p>Texto de la tarjeta</p>
+          <h3>Super Gato 4</h3>
+          <p>El gato que siempre está en movimiento</p>
         </div>
       </div>
 
