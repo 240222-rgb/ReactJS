b455fbcde9ec5cd02cbb45df06ccfc3d58d0bfae
Joanna Itzel Perez Ordaz <240222@utxicotepec.edu.mx>
2026-02-12 14:32:20 -0600

    add: se agrego la api

--- my-react-app/src/Rectangulo.jsx
+++ my-react-app/src/Rectangulo.jsx
@@ -1,3 +1,4 @@
+import Mapa from './Mapa';
 import "./Rectangulo.css";
 
 function Rectangulo() {
@@ -6,6 +7,11 @@ function Rectangulo() {
       <div className="rectangulo-overlay">
         <h2>Diseño Creativo</h2>
         <p>Ideas visuales que conectan con tu marca</p>
+        <Mapa 
+          lat = {20.2682762109786}
+          lng = {-97.9572129259522}
+          nombre_sucursal={"Sucursal Norte"}
+        />
       </div>
     </section>
   );
